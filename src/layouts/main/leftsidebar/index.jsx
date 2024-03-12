386e2661f9f1768439c964f36d06be64ef0562fb
khazarglzd <khazarguluzade@gmail.com>
2024-03-12 19:06:34 +0400

    creating Menu component & adding classNames packge

--- src/layouts/main/leftsidebar/index.jsx
+++ src/layouts/main/leftsidebar/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Logo from './Logo'
+import Menu from './Menu'
 
 const LeftSideBar = () => {
     return (
         <aside className="w-[275px] min-h-screen px-2">
             <Logo />
+            <Menu />
         </aside>
     )
 }
