000e38bdcc3889c85e242cf4738cb43435e89ba4
khazarglzd <khazarguluzade@gmail.com>
2024-03-18 21:21:17 +0400

    creating authentication slice for redux

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import routes from "./routes"
 import "./assets/css/tailwind.css"
+import store from "../src/store"
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={routes} />
+  <Provider store={store}>
+    <RouterProvider router={routes} />
+  </Provider>
 );
 
