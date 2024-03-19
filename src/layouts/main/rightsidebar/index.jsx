6a331f2eecbf54a973646e1aa21623cdfdcdb273
khazarglzd <khazarguluzade@gmail.com>
2024-03-20 01:48:35 +0400

    Premium component for RightSideBar

--- src/layouts/main/rightsidebar/index.jsx
+++ src/layouts/main/rightsidebar/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Premium from './premium'
 import Search from './search'
 
 const RightSideBar = () => {
     return (
         <aside className="w-[350px] mr-2.5">
             <Search />
+            <Premium />
 
         </aside>
     )
