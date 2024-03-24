d02a3f305c2149c17f1c7a99a77f270b138aa740
khazarglzd <khazarguluzade@gmail.com>
2024-03-24 20:32:04 +0400

    refactoring

--- src/layouts/main/rightsidebar/index.jsx
+++ src/layouts/main/rightsidebar/index.jsx
@@ -3,6 +3,7 @@ import Premium from './premium'
 import Search from './search'
 import Topics from './topics'
 
+
 const RightSideBar = () => {
     return (
         <aside className="w-[350px] mr-2.5">
