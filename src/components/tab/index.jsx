2115a30488b326191287d82cb6badb1dd8b15a62
khazarglzd <khazarguluzade@gmail.com>
2024-03-26 17:39:51 +0400

    creating StickyHeader component for Home

---
+++ src/components/tab/index.jsx
@@ -0,0 +1,20 @@
+import Items from "./items";
+import Item from "./item";
+import Content from "./content";
+
+
+
+export default function Tab({ children, }) {
+
+
+    return (
+        <div>
+            tab
+        </div>
+    )
+}
+
+
+Tab.Items = Items
+Tab.Item = Item
+Tab.Content = Content
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import StickyHeader from '../../components/sticky-header'
+import Tab from '../../components/tab/index'
 
 const Home = () => {
     return (
-        <div>Home page</div>
+        <div>
+            <Tab activeTab="followings">
+                <StickyHeader title="Anasayfa">
+                    <Tab.Items>
+                        <Tab.Item id="for-you">
+                            Sana özel
+                        </Tab.Item>
+                        <Tab.Item id="followings">
+                            Takip edilenler
+                        </Tab.Item>
+                    </Tab.Items>
+                </StickyHeader>
+                <Tab.Content id="for-you">
+                    1. content
+                </Tab.Content>
+                <Tab.Content id="followings">
+                    2. content
+                </Tab.Content>
+            </Tab>
+        </div>
     )
 }
 
