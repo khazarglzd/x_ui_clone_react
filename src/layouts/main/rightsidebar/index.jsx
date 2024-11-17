ecd142ecd7c18a80c3f0809cd3b5c685937eb627
khazarglzd <khazarguluzade@gmail.com>
2024-11-17 14:28:19 +0400

    making RightSideBar sticky

--- src/layouts/main/rightsidebar/index.jsx
+++ src/layouts/main/rightsidebar/index.jsx
@@ -3,17 +3,20 @@ import Footer from './footer'
 import Premium from './premium'
 import Search from './search'
 import Topics from './topics'
+import StickyBox from "react-sticky-box";
 
 
 const RightSideBar = () => {
     return (
-        <aside className="w-[350px] mr-2.5">
-            <Search />
-            <Premium />
-            <Topics />
-            <Footer />
+        <StickyBox className='self-start' offsetBottom={80}>
+            <aside className="w-[350px] mr-2.5">
+                <Search />
+                <Premium />
+                <Topics />
+                <Footer />
+            </aside>
+        </StickyBox>
 
-        </aside>
     )
 }
 
